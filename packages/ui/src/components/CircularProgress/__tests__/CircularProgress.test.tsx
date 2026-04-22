c589ab397ac310b9b55b09836b9b73ef8702d414
Peanut <truongdq.dev@gmail.com>
2026-04-22 20:39:02 +0700

    docs: add TODO comment to CircularProgress test
    - Added detailed TODO comment explaining react-native-svg Touchable.Mixin error
    - Listed possible fixes for the issue
    - Test remains skipped until issue is resolved

--- packages/ui/src/components/CircularProgress/__tests__/CircularProgress.test.tsx
+++ packages/ui/src/components/CircularProgress/__tests__/CircularProgress.test.tsx
@@ -3,6 +3,13 @@ import { ThemeProvider } from '@truongdq01/headless';
 import React from 'react';
 import { CircularProgress } from '../CircularProgress';
 
+// TODO: Fix react-native-svg Touchable.Mixin error
+// Error occurs because react-native-svg@15.15.4's internal code accesses actual react-native module before mocks take effect
+// Possible fixes:
+// 1. Downgrade react-native-svg to a version without this issue
+// 2. Patch react-native-svg package
+// 3. Switch to a different SVG library
+// 4. Switch from Bun to npm/yarn for testing (might avoid hoisting issue)
 test.skip('CircularProgress renders - react-native-svg Touchable.Mixin error cannot be fixed with mocking', () => {
   const { toJSON } = render(
     <ThemeProvider>
